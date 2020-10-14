971957a0dd48793375f398977da1d077911a3d67
Mike Giebner <mikegiebner@gmail.com>
2020-10-14 12:05:29 -0400

    C21-6 is complete

---
+++ client/src/components/Product.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import PropTypes from "prop-types";
+
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="div">
+          <div className="my-3">
+            {product.rating} from {product.numReviews} reviews
+          </div>
+        </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+Product.propTypes = { product: PropTypes.object };
+
+export default Product;
