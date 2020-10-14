e55c44526076cfe05939f37622c9d9fbdc2d7392
Mike Giebner <mikegiebner@gmail.com>
2020-10-14 10:44:34 -0400

    C21-5 is complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
